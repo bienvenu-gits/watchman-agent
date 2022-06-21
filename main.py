54e574a01ed985404afb179ac661212758bcd00f
Flobert GANTUA <fgantua32@gmail.com>
2022-06-21 15:25:07 +0100

    Feat:Deploy agent 1.1.8

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ def getting_stacks_by_host_snmp(active_hosts,community):
 
         commande_output = subprocess.getstatusoutput("snmpwalk -v1 -c %s %s .1.3.6.1.2.1.1.1.0" %(community, host))
         os_info = re.search('"(*.)"',commande_output[1])
+        os_info = os_info.group(1)
         
         if len(os_info) >= 50: 
             os_info = os_info.split("#")[1]
