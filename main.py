34127cf05c629fd5166ea5dd592ccae50924475b
Carlos AGBOTON <agboton.carlos@gits.bj>
2023-10-03 16:57:04 +0100

    - chore: fix merge

--- main.py
+++ main.py
@@ -292,6 +292,135 @@ def is_ip_active(ip, all_active=False):
             return False
 
 
+# def is_ip_active(ip, all_active=False):
+#     try:
+#         # Exécutez la commande de ping
+#         result = subprocess.run(['ping', '-c', '1', '-w', '5', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+#                                 text=True)
+#         # Vérifiez le code de retour pour déterminer si le ping a réussi
+#         if result.returncode == 0:
+#             # Le ping a réussi, l'adresse IP est active
+#             return True
+#         else:
+#             # Le ping a échoué, l'adresse IP est inactive
+#             if all_active:
+#                 # On considère tous les hosts comme active
+#                 return True
+#             else:
+#                 return False
+#     except Exception as e:
+#         print(e)
+#         # Une erreur s'est produite, l'adresse IP est probablement inactive
+#         return False
+
+
+linux_version_pattern = re.compile(
+    r"^"
+    # epoch must start with a digit
+    r"(\d+:)?"
+    # upstream must start with a digit
+    r"\d"
+    r"("
+    # upstream  can contain only alphanumerics and the characters . + -
+    # ~ (full stop, plus, hyphen, tilde)
+    r"[A-Za-z0-9\.\+\~\-]+"
+    r"|"
+    # If there is no debian_revision then hyphens are not allowed in version.
+    r"[A-Za-z0-9\.\+\~]+-[A-Za-z0-9\+\.\~]+"
+    r")?"
+    r"$"
+)
+
+irregular_version_pattern = re.compile(r'\d+(\.\d+)*')
+
+
+def parse_version(text):
+    """ Semantic Versioning (SemVer)
+     Date-based Versioning
+     Alphanumeric or Custom Schemes
+     Debian based version parser
+     Ubuntu based version parser
+     parse version with build:
+    """
+    if linux_version_pattern.match(text):
+        match = linux_version_pattern.search(text)
+        if match:
+            version = match.group()
+            if ":" in version:
+                epoch, _, version = version.partition(":")
+                epoch = int(epoch)
+            else:
+                epoch = 0
+
+            if "-" in version:
+                upstream, _, revision = version.rpartition("-")
+            else:
+                upstream = version
+                revision = "0"
+
+            version = upstream
+            regex_matched = False
+
+            if 'ubuntu' in version:
+                match = irregular_version_pattern.search(version)
+                if match:
+                    regex_matched = True
+                    version = match.group()
+            elif 'debian' in version:
+                match = irregular_version_pattern.search(version)
+                if match:
+                    regex_matched = True
+                    version = match.group()
+            elif 'git' in version:
+                match = irregular_version_pattern.search(version)
+                if match:
+                    regex_matched = True
+                    version = match.group()
+            elif '-' in version:
+                match = irregular_version_pattern.search(version)
+                if match:
+                    regex_matched = True
+                    version = match.group()
+            else:
+                match = irregular_version_pattern.search(version)
+                if match:
+                    regex_matched = True
+                    version = match.group()
+
+            parsed = None
+            if not regex_matched:
+                try:
+                    parsed = sem_version.parse(version)
+                except ValueError:
+                    try:
+                        parsed = pkg_version.parse(version)
+                    except pkg_version.InvalidVersion:
+                        parsed = None
+
+            if parsed:
+                parsed_split_len = len(str(parsed).split("."))
+                if parsed_split_len < 3:
+                    version = [str(parsed.major), str(parsed.minor)]
+                elif parsed_split_len == 3:
+                    try:
+                        version = [str(parsed.major), str(parsed.minor), str(parsed.patch)]
+                    except AttributeError:
+                        version = [str(parsed.major), str(parsed.minor), str(parsed.micro)]
+                else:
+                    version = parsed
+
+                if isinstance(version, list):
+                    version = ".".join(version)
+                else:
+                    version = version
+            else:
+                if not regex_matched:
+                    print(f'Cannot definitely parse version {text}')
+
+            # print(f"Final parsed version: {version}")
+            return version
+
+
 def snmp_scanner(ip, ports: list = None):
     if ports is None:
         ports = [161]
@@ -513,11 +642,11 @@ async def getting_stacks_by_host_snmp(active_hosts, community):
         start = time.perf_counter()
         packages = await get_packages_async(host, community, installed_packages_bind)
         end = time.perf_counter()
-        print(f"getting packages took: {end-start}")
+        print(f"getting packages took: {(end-start)/60:.2f} minutes")
         start = time.perf_counter()
         os_info = await get_host_info_async(host, community, sys_info_bind)
         end = time.perf_counter()
-        print(f"getting host infos took: {end-start}")
+        print(f"getting host infos took: {(end-start)/60:.2f} minutes")
 
         # print(os_info)
         # print(packages)
@@ -526,6 +655,8 @@ async def getting_stacks_by_host_snmp(active_hosts, community):
             "ipv4": host,
             "packages": packages
         }
+
+        # print(f"out hosts_report {hosts_report}")
     return json.dumps(hosts_report)
 
 
