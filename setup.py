fc9b1151bf227ba5ba4c0b7a90cdba1838d17c98
Carlos AGBOTON <agboton.carlos@gits.bj>
2023-11-20 18:06:09 +0100

    `feat`: agent V2 post deployment

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 setup(
     name="watchman-agent",
@@ -6,7 +6,10 @@ setup(
     author="Watchman",
     author_email="support@watchman.bj",
     # description = "Watchman Agent 1.0.0",
-    packages=['watchman_agent', 'watchman_agent.*'],
+    packages=find_packages(
+        where='watchman_agent',
+        include=['watchman_agent.*']
+    ),
     python_requires='>=3.8',
     include_package_data=True,
     # py_modules=[''],
