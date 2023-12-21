f87c9296c4e9eaddb6c948950a350cb09db4d083
Carlos AGBOTON <agboton.carlos@gits.bj>
2023-12-21 01:56:54 +0100

    `deploy`: deploying linux

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="watchman-agent",
-    version="2.0.9",
+    version="2.1.0",
     author="Watchman",
     author_email="support@watchman.bj",
     # description = "Watchman Agent 1.0.0",
